0274f5a8a9fc5c90cefbe4289c05a8002d8c825f
Francisco Ramos Bisneto <francisco.frb@dpf.gov.br>
2020-10-23 22:11:44 -0300

    Mockando front.

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,20 +1,107 @@
 (function () {
-    "use strict";
+  'use strict';
 
-    angular.module('todoApp')
-        .controller('homeController', homeController);
+  angular.module('todoApp')
+    .controller('homeController', homeController);
 
-    homeController.$inject = ['helperFactory', 'todoService'];
+  homeController.$inject = ['constantes', 'todoService', '$timeout'];
 
-    function homeController(helper, service) {
-        var vm = this;
-        vm.teste = 'Mensagem teste'
-        /* ***************    INIT VARIÁVEIS    *********************************** */
+  function homeController(constantes, service, $timeout) {
+    var vm = this;
+    /* ***************    INIT VARIÁVEIS    *********************************** */
+    vm.teste = 'Testando';
+    vm.cards = [];
 
-        /* ***************    FUNÇÕES EXECUTADAS NA VIEW (HMTL)    **************** */
+    /* ***************    INDICE FUNÇÕES    **************** */
+    vm.init = init;
+    vm.criarCards = criarCards;
+    vm.dragEvent = dragEvent;
+    vm.drop = drop;
+    vm.allowDrop = allowDrop;
+    vm.loading = true;
 
-        /* ***************    FUNÇÕES INTERNAS    ******************************** */
+    /* ***************    FUNÇÕES INTERNAS    ******************************** */
 
+    function init() {
+      vm.criarCards();
+      $timeout(() => {
+        console.log('timeout');
+        vm.loading = false;
+      }, 2000);
     }
+
+    function criarCards() {
+      vm.cards = [
+        {
+          idStatus: 1,
+          status: 'TODO',
+          severity: 'info',
+          registros: [
+            {
+              id: 1,
+              text: 'Prova Quarkus'
+            }
+          ]
+        },
+        {
+          idStatus: 2,
+          status: 'DOING',
+          severity: 'warning',
+          registros: [
+            {
+              id: 2,
+              text: 'Estudando'
+            },
+            {
+              id: 3,
+              text: 'Fazendo front-end'
+            },
+          ]
+        },
+        {
+          idStatus: 3,
+          status: 'DONE',
+          severity: 'success',
+          registros: [
+            {
+              id: 4,
+              text: 'Prova AngularJS'
+            },
+            {
+              id: 5,
+              text: 'Prova Java'
+            },
+          ]
+        },
+        {
+          idStatus: 4,
+          status: 'BLOCK',
+          severity: 'danger',
+          registros: [
+            {
+              id: 6,
+              text: 'Fazer fork no projeto final'
+            }
+          ]
+        }
+      ];
+    }
+
+    function dragEvent(event, sourceCard, value) {
+      console.log('source:', sourceCard);
+      console.log('valor source:', value);
+      event.dataTransfer.setData('dragged', JSON.stringify(value));
+    }
+
+    function drop(event, targetCard) {
+      console.log('target:', targetCard);
+      var data = JSON.parse(event.dataTransfer.getData('dragged'));
+      console.log('valor target:', data);
+    }
+
+    function allowDrop(event) {
+      event.preventDefault();
+    }
+  }
 
 })();
--- app/utilities/constantes.js
+++ app/utilities/constantes.js
@@ -1,16 +1,9 @@
 (function () {
-    "use strict";
+  "use strict";
 
-    angular.module('todoApp')
-        .constant('constantes', {
-            URL_BASE: 'https://gitmaratonadev.stefanini.com.br/apisemanaangular/api',
-            // Podemos também colocar objetos dentro de objetos, veja como é usado
-            // no helper.factory para enviar msg de SEM_ACESSO
-            MENSAGENS: {
-                ERRO_GERAL: 'Ocorreu algum problema. Tente novamente mais tarde.',
-                SEM_ACESSO: 'Você não tem acesso a esta página.'
-            },
-            MSG_ERRO: 'Ocorreu algum problema. Tente novamente mais tarde.',
-        });
+  angular.module('todoApp')
+    .constant('constantes', {
+      URL_BASE: 'http://localhost:8080/api/todo'
+    });
 
 })();
